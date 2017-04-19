5c2ce5427a14c67d5875b24d50a7835fea0e17cc
Brad Richardson <brichardson@structint.com>
2017-04-19 12:14:27 -0700

    Some of the snippets in Ch 3

---
+++ ch03/BookStore.hs
@@ -0,0 +1,26 @@
+data BookInfo = Book Int String [String]
+                deriving (Show)
+
+data MagazineInfo = Magazine Int String [String]
+                    deriving (Show)
+
+myInfo = Book 9780135072455 "Algebra of Programming"
+         ["Richard Bird", "Oege de Moor"]
+
+data BookReview = BookReview BookInfo CustomerID String
+
+type CustomerID = Int
+type ReviewBody = String
+
+data BetterReview = BetterReview BookInfo CustomerID ReviewBody
+
+type BookRecord = (BookInfo, BookReview)
+
+type CardHolder = String
+type CardNumber = String
+type Address = [String]
+
+data BillingInfo = CreditCard CardNumber CardHolder Address
+                 | CashOnDelivery
+                 | Invoice CustomerID
+                   deriving (Show)
