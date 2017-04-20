4189cad10c0ff61644e5f0be726118682bc799f4
Brad Richardson <brichardson@structint.com>
2017-04-20 12:27:25 -0700

    Solve exercises in Ch 3

--- ch03/ListADT.hs
+++ ch03/ListADT.hs
@@ -4,3 +4,6 @@ data List a = Cons a (List a)
 
 fromList (x:xs) = Cons x (fromList xs)
 fromList []     = Nil
+
+toList (Cons x y) = x:(toList y)
+toList Nil = []
--- ch03/Tree.hs
+++ ch03/Tree.hs
@@ -4,3 +4,8 @@ data Tree a = Node a (Tree a) (Tree a)
 
 simpleTree = Node "parent" (Node "left child" Empty Empty)
                            (Node "right child" Empty Empty)
+
+data OtherTree a = OtherNode a (Maybe (OtherTree a)) (Maybe (OtherTree a)) deriving (Show)
+
+differentTree = OtherNode "parent" (Just (OtherNode "left child" Nothing Nothing))
+                                   (Just (OtherNode "right child" Nothing Nothing))
