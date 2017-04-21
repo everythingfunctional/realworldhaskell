d42062d629faed7da1f90902687304d5f582b689
Brad Richardson <brichardson@structint.com>
2017-04-21 12:13:40 -0700

    More snippets from ch03

---
+++ ch03/BogusPattern.hs
@@ -0,0 +1,17 @@
+data Fruit = Apple | Orange
+
+apple = "apple"
+
+orange = "orange"
+
+whichFruit :: String -> Fruit
+whichFruit f = case f of
+                 apple -> Apple
+                 orange -> Orange
+
+equational apple = Apple
+equational orange = Orange
+
+betterFruit f = case f of
+                  "apple" -> Apple
+                  "orange" -> Orange
---
+++ ch03/GoodIndent.hs
@@ -0,0 +1,7 @@
+-- This is the leftmost column.
+
+  -- It's fine for top-level declarations to start in any column...
+  firstGoodIndentation = 1
+
+  -- ...provided all subsequent declarations do, too!
+  secondGoodIndentation = 2
