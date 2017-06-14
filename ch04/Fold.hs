b83b3295910bb806a7172d7cb1060a441c47d1af
Brad Richardson <brichardson@structint.com>
2017-06-14 12:26:39 -0700

    A couple more examples using foldr

--- ch04/Fold.hs
+++ ch04/Fold.hs
@@ -42,3 +42,9 @@ myFoldl :: (a -> b -> a) -> a -> [b] -> a
 
 myFoldl f z xs = foldr step id xs z
     where step x g a = g (f a x)
+
+identity :: [a] -> [a]
+identity xs = foldr (:) [] xs
+
+append :: [a] -> [a] -> [a]
+append xs ys = foldr (:) ys xs
