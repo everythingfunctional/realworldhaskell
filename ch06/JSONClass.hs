789caf77f9b1cadb3bf329dc649a9c1cafac2b65
Brad Richardson <brichardson@structint.com>
2017-10-09 12:50:08 -0700

    Borked typeclasses

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE TypeSynonymInstances #-}
 
+import SimpleJSON
+
 type JSONError = String
 
 class JSON a where
@@ -20,3 +22,19 @@ instance JSON String where
 
     fromJValue (JString s) = Right s
     fromJValue _           = Left "not a JSON string"
+
+doubleToJValue :: (Double -> a) -> JValue -> Either JSONError a
+doubleToJValue f (JNumber v) = Right (f v)
+doubleToJValue _ _ = Left "not a JSON number"
+
+instance JSON Int where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Integer where
+    toJValue = JNumber . realToFrac
+    fromJValue = doubleToJValue round
+
+instance JSON Double where
+    toJValue = JNumber
+    fromJValue = doubleToJValue id
