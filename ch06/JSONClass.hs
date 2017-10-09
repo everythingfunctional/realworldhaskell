deafdb98f81448cbc35f70f95e930d0b9ca65e11
Brad Richardson <brichardson@structint.com>
2017-10-09 12:25:13 -0700

    More example instances

--- ch06/JSONClass.hs
+++ ch06/JSONClass.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+
 type JSONError = String
 
 class JSON a where
@@ -7,3 +9,14 @@ class JSON a where
 instance JSON JValue where
     toJValue = id
     fromJValue = Right
+
+instance JSON Bool where
+    toJValue = JBool
+    fromJValue (JBool b) = Right b
+    fromJValue _ = Left "not a JSON boolean"
+
+instance JSON String where
+    toJValue               = JString
+
+    fromJValue (JString s) = Right s
+    fromJValue _           = Left "not a JSON string"
