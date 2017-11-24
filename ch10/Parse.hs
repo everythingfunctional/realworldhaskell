fb9aa6301b76750c54ae3a8892c6300b7f63a0d0
Brad Richardson <brichardson@structint.com>
2017-11-24 11:43:02 -0800

    Implement simple parser

--- ch10/Parse.hs
+++ ch10/Parse.hs
@@ -1,5 +1,6 @@
 import qualified Data.ByteString.Lazy as L
 import           Data.Int
+import           Data.Word            (Word8)
 
 data ParseState = ParseState {
       string :: L.ByteString
@@ -28,3 +29,35 @@ parse parser initState
 modifyOffset :: ParseState -> Int64 -> ParseState
 modifyOffset initState newOffset =
     initState { offset = newOffset }
+
+parseByte :: Parse Word8
+parseByte =
+    getState ==> \initState ->
+    case L.uncons (string initState) of
+        Nothing ->
+            bail "no more input"
+        Just (byte, remainder) ->
+            putState newState ==> \_ ->
+            identity byte
+          where newState = initState { string = remainder,
+                                       offset = newOffset }
+                newOffset = offset initState + 1
+
+getState :: Parse ParseState
+getState = Parse (\s -> Right (s, s))
+
+putState :: ParseState -> Parse ()
+putState s = Parse (\_ -> Right ((), s))
+
+bail :: String -> Parse a
+bail err = Parse $ \s -> Left $
+           "byte offset " ++ show (offset s) ++ ": " ++ err
+
+(==>) :: Parse a -> (a -> Parse b) -> Parse b
+firstParser ==> secondParser = Parse chainedParser
+    where chainedParser initState =
+            case runParse firstParser initState of
+                Left errMessage ->
+                    Left errMessage
+                Right (firstResult, newState) ->
+                    runParse (secondParser firstResult) newState
