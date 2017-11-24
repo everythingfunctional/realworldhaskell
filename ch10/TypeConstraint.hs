39087b4d62a1e0ece72a2b58f26d17cf256b8426
Brad Richardson <brichardson@structint.com>
2017-11-24 12:03:06 -0800

    Illustrate bad use of Type Constraints

---
+++ ch10/TypeConstraint.hs
@@ -0,0 +1,12 @@
+data (Ord a) => OrdStack a = Bottom
+                           | Item a (Ord Stack a)
+                             deriving (Show)
+
+isIncreasing :: (Ord a) => OrdStack a -> Bool
+isIncreasing (Item a rest@(Item b _))
+    | a < b = isIncreasing rest
+    | otherwise = False
+isIncreasing _ = True
+
+push :: (Ord a) => a -> OrdStack a -> OrdStack a
+push a s = Item a s
---
+++ ch10/ValidFunctor.hs
@@ -0,0 +1,9 @@
+data Foo a = Foo a
+
+instance Functor Foo where
+    fmap f (Foo a) = Foo (f a)
+
+data Eq a => Bar a = Bar a
+
+instance Functor Bar where
+    fmap f (Bar a) = Bar (f a)
